0946de4a9c1f38af74ae57d32c3b974736163953
Danilo Righetto <drighetto@despachantedok.com.br>
2024-11-17 22:39:54 -0300

    Configurando o projeto

--- api/app/Http/Controllers/Controller.php
+++ api/app/Http/Controllers/Controller.php
@@ -2,6 +2,17 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="Exatait-Test",
+ *      description="Webservice responsável pelo gerenciamento de tarefas",
+ *      @OA\Contact(
+ *          name="Exatait",
+ *          url="https://www.exata.it"
+ *      )
+ * )
+ */
 abstract class Controller
 {
     //
--- api/resources/js/app.js
+++ api/resources/js/app.js
@@ -1,1 +1,7 @@
 import './bootstrap';
+import { createApp } from 'vue';
+import App from './components/App.vue'
+
+const app = createApp();
+app.component('app', App);
+app.mount('#app');
