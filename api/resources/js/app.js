83a57615d7a4200abbb54f72e11de7141d5e736e
Danilo Righetto <drighetto@despachantedok.com.br>
2024-11-19 13:20:17 -0300

    Criando paginas iniciais

--- api/resources/js/app.js
+++
@@ -1,7 +0,0 @@
-import './bootstrap';
-import { createApp } from 'vue';
-import App from './components/App.vue'
-
-const app = createApp();
-app.component('app', App);
-app.mount('#app');
--- api/routes/api.php
+++ api/routes/api.php
@@ -1,17 +1,7 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
 Route::get('/health/live', function () {
     return response()->json(['statusAplication' => 'Healthy', 'currentTime' => date('Y-m-d H:i:s'), 'checks' => []]);
@@ -20,3 +10,7 @@ Route::get('/health/live', function () {
 Route::get('/health/ready', function () {
     return response()->json(['statusAplication' => 'Healthy', 'currentTime' => date('Y-m-d H:i:s'), 'checks' => []]);
 });
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
